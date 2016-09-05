d56a2421d109529b7845c507cc919334ffee5111
JisuPark <jisu6293@naver.com>
2016-09-05 13:48:04 +0900

    2. babel-polyfill applied

--- production.config.js
+++ production.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    App: [path.join(__dirname, '/public/scripts/example.js')]
+    App: ['babel-polyfill', path.join(__dirname, '/public/scripts/example.js')]
   },
   output: {
     path: path.join(__dirname,'/public/scripts'),
